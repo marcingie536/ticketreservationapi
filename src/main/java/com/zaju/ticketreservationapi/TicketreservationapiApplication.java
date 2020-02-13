71229976d2cb18e5ed5817a13f248c764b8eab0c
marcingie536 <marcin.g536@gmail.com>
2020-02-13 17:02:38 +0100

    initial

---
+++ src/main/java/com/zaju/ticketreservationapi/TicketreservationapiApplication.java
@@ -0,0 +1,13 @@
+package com.zaju.ticketreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketreservationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketreservationapiApplication.class, args);
+	}
+
+}
